f4e24cd5c4af6a3c602c1c37fe24556f8cdb2e81
JoyBoy369 <acholucas136@gmail.com>
2024-06-02 20:25:27 -0300

    creacion de Layaouts y remplazo en HTML

--- tp4_AchoLucas/src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ tp4_AchoLucas/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -1,5 +1,16 @@
 package ar.edu.unju.fi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
 public class MainController {
+	
+	@GetMapping("/inicio")
+	public String getIndexPage(Model model) {
+		return "index";
+	}
 
 }
